e79ccdcdd150bfec97ed7c09ef014f4899678ccb
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-20 16:34:49 -0600

    Agregar migraciones

--- MagicVilla_API/Data/ApplicationDbContext.cs
+++ MagicVilla_API/Data/ApplicationDbContext.cs
@@ -5,6 +5,11 @@ namespace MagicVilla_API.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
         public DbSet<Villa> Villas { get; set; }
 
 
