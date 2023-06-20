a51df71e22fc6f2f207678643baf9381b49cedcf
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-20 15:39:52 -0600

    Paquetes de datos

--- MagicVilla_API/Models/Dto/VillaDto.cs
+++ MagicVilla_API/Models/Dto/VillaDto.cs
@@ -10,8 +10,17 @@ namespace MagicVilla_API.Models.Dto
         [MaxLength(30)]
         public string Nombre { get; set; }
 
-        public int Ocupantes { get; set; }
+        public string Detalle { get; set; }
 
-        public int MetrosCuadrados { get; set; }
+        [Required]
+        public double Tarifa { get; set; }
+
+        public int Ocupantes { get; set; }
+        public double MetrosCuadrados { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        public string Amenidad { get; set; }
+
     }
 }
--- MagicVilla_API/Models/Villa.cs
+++ MagicVilla_API/Models/Villa.cs
@@ -1,11 +1,29 @@
-﻿namespace MagicVilla_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Models
 {
     public class Villa
     {
+        [Key]
         public int Id { get; set; }
 
         public string Nombre { get; set; }
 
+        public string Detalle { get; set; }
+
+        [Required]
+        public double Tarifa { get; set; }
+
+        public int Ocupantes { get; set; }
+
+        public double MetrosCuadrados { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        public string Amenidad { get; set; }
+
         public DateTime FechaCreacion { get; set; }
+
+        public DateTime FechaActualiazcion { get; set; }
     }
 }
