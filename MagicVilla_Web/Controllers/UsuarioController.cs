02198a51c9205fa687ba56fef9229c408aee0e1b
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-22 13:38:54 -0600

    vistas

---
+++ MagicVilla_Web/Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginRequestDto modelo)
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registrar(RegistroRequestDto modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _usuarioService.Registrar<APIResponse>(modelo);
+
+                if (response is not null && response.IsExitoso)
+                {
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            return View(modelo);
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+    }
+}
--- MagicVilla_Web/Models/Dto/RegistroRequestDto.cs
+++ MagicVilla_Web/Models/Dto/RegistroRequestDto.cs
@@ -1,13 +1,19 @@
-﻿namespace MagicVilla_Web.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.Dto
 {
     public class RegistroRequestDto 
     {
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
         public string Nombres { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
+        [Required]
         public string Rol { get; set; }
     }
 }
