835562a2b7adf324ab78b9b75b81ea6117042067
Carlos Perez Salazar <cps172779@gmail.com>
2023-06-21 18:09:58 -0600

    Proyecto web dtos y modelos api

---
+++ MagicVilla_Web/Models/Dto/NumeroVillaUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.Dto
+{
+    public class NumeroVillaUpdateDto
+    {
+        [Required]
+        public int VillaNo { get; set; }
+
+        [Required]
+        public int VillaId { get; set; }
+
+        public string DetallesEspeciales { get; set; }
+
+    }
+}
